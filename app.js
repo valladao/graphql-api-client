0be514492bb2615737783a1dc1532012612c2f9a
Manoel Valladao <valladao@gmail.com>
2021-06-16 20:19:12 -0300

    Fetch a set of data from the GitHub API

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express")
-const app = express()
+const fetch = require("node-fetch")
 
+const app = express()
 app.use(express.static("public"))
 
-app.get("/data", (req, res) => {
-  res.end("Response from the API!")
+app.get("/data", async (req, res) => {
+  const query = `{ viewer { login } }`
+  const url = "https://api.github.com/graphql"
+  let response
+
+  const options = {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+      authorization: "bearer " + process.env.APIKEY,
+    },
+    body: JSON.stringify({ query: query }),
+  }
+
+  try {
+    response = await fetch(url, options)
+  } catch (error) {
+    console.error(error)
+  }
+  const data = await response.json()
+  console.log(data)
+
+  res.json(data)
 })
 
 app.listen(3000, () => console.log("Server ready"))
--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const response = await fetch("/data")
-  const text = await response.text()
-  alert(text)
+  const data = await response.json()
+  console.log(data)
 })
